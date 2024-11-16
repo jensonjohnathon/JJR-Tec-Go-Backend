a6fc8a41afe80c7264c52d77320f52927ab68f64
johnathon <jenson.j.reggentin@hochschule-stralsund.de>
2024-11-16 19:30:56 +0100

    clean up

--- migrations/1000_seed_default_data.up.sql
+++ migrations/1000_seed_default_data.up.sql
@@ -1,16 +1,10 @@
--- Make sure to update this password manually or in migrations if it changes
-
 -- Enable pgcrypto extension for password encryption
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
-
+-- Make sure to update this password manually or in migrations if it changes
 INSERT INTO users (username, email, password)
 VALUES ('admin', 'admin@example.com', crypt('DefaultAdminPassword', gen_salt('bf', 12)))
 ON CONFLICT DO NOTHING;
-
-/* INSERT INTO users (username, email, password)
-VALUES ('admin', 'admin@example.com', 'DefaultAdminPassword')
-ON CONFLICT DO NOTHING; */
 
 -- Assign admin role to admin user
 INSERT INTO user_roles (user_id, role_id)
